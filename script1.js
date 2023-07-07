c602fc1248af9e0726bc7dac80ae9ac5c2d912b9
same-hack <samehack.info@gmail.com>
2023-07-07 14:51:26 +0900

    ad

--- script1.js
+++ script1.js
@@ -19,21 +19,39 @@ function handleFile() {
         processEntries(zip, "", fileObjects).then(function () {
           console.log(fileObjects);
 
+          // 結果を格納するオブジェクト
           const result = {};
+
+          // 配列を順番に処理
           for (const obj of fileObjects) {
+            // ファイルパスをディレクトリとファイル名に分割
             const [dir, file] = obj.path.split("/");
+
+            // ディレクトリがまだ結果に含まれていない場合、空の配列を作成して初期化
             if (!result[dir]) {
               result[dir] = [];
             }
+
+            // ファイル名をディレクトリの配列に追加
             result[dir].push(file);
           }
 
-          const transformedArray = Object.entries(result).map(([dir, files]) => ({
-            [dir]: {
-              files,
-              count: files.length,
-            },
-          }));
+          // 変換された配列を格納するための変数
+          const transformedArray = [];
+
+          // 結果オブジェクトを走査
+          for (const [dir, files] of Object.entries(result)) {
+            // ディレクトリごとにオブジェクトを作成し、ファイル名とファイル数を追加
+            const dirObject = {
+              [dir]: {
+                files,
+                count: files.length,
+              },
+            };
+
+            // 変換された配列にオブジェクトを追加
+            transformedArray.push(dirObject);
+          }
 
           console.log(transformedArray);
         });
