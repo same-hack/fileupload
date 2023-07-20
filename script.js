dc670200c3e47f1942a94dfa947b0f84da5722f8
same-hack <samehack.info@gmail.com>
2023-07-20 13:57:23 +0900

    ad

--- script.js
+++ script.js
@@ -44,47 +44,47 @@ document.head.appendChild(script);
 //   reader.readAsArrayBuffer(file);
 // }
 
-function handleFile() {
-  var fileInput = document.getElementById("zipFileInput");
-  var file = fileInput.files[0];
+// function handleFile() {
+//   var fileInput = document.getElementById("zipFileInput");
+//   var file = fileInput.files[0];
 
-  var reader = new FileReader();
-  reader.onload = function (event) {
-    var arrayBuffer = event.target.result;
-    var jszip = new JSZip();
+//   var reader = new FileReader();
+//   reader.onload = function (event) {
+//     var arrayBuffer = event.target.result;
+//     var jszip = new JSZip();
 
-    jszip
-      .loadAsync(arrayBuffer)
-      .then(function (zip) {
-        var filePromises = [];
+//     jszip
+//       .loadAsync(arrayBuffer)
+//       .then(function (zip) {
+//         var filePromises = [];
 
-        Object.keys(zip.files).forEach(function (filename) {
-          var file = zip.files[filename];
+//         Object.keys(zip.files).forEach(function (filename) {
+//           var file = zip.files[filename];
 
-          filePromises.push(
-            file.async("uint8array").then(function (data) {
-              var decoder = new TextDecoder("utf-8");
-              var content = decoder.decode(data);
+//           filePromises.push(
+//             file.async("uint8array").then(function (data) {
+//               var decoder = new TextDecoder("utf-8");
+//               var content = decoder.decode(data);
 
-              return {
-                filename: filename,
-                content: content,
-              };
-            })
-          );
-        });
+//               return {
+//                 filename: filename,
+//                 content: content,
+//               };
+//             })
+//           );
+//         });
 
-        Promise.all(filePromises).then(function (fileObjects) {
-          console.log(fileObjects);
-        });
-      })
-      .catch(function (error) {
-        console.error("解凍エラー:", error);
-      });
-  };
+//         Promise.all(filePromises).then(function (fileObjects) {
+//           console.log(fileObjects);
+//         });
+//       })
+//       .catch(function (error) {
+//         console.error("解凍エラー:", error);
+//       });
+//   };
 
-  reader.readAsArrayBuffer(file);
-}
+//   reader.readAsArrayBuffer(file);
+// }
 
 // function handleFile() {
 //   var fileInput = document.getElementById("zipFileInput");
@@ -129,3 +129,65 @@ function handleFile() {
 
 //   reader.readAsArrayBuffer(file);
 // }
+
+filesByDirectory: {
+  path: string;
+}
+[] = [];
+
+function handleFileInput(event) {
+  const file = event.target.files[0];
+
+  if (file) {
+    this.unzipFile(file);
+  }
+}
+
+function unzipFile(file) {
+  const zip = new JSZip();
+
+  // ファイルを読み込む
+  zip.loadAsync(file).then((zipContent) => {
+    // 解凍されたファイルを処理する
+    zipContent.forEach((relativePath, file) => {
+      if (!file.dir) {
+        const fileEntry = { path: relativePath };
+
+        // ディレクトリごとにファイルを追加する
+        this.filesByDirectory.push(fileEntry);
+      }
+    });
+
+    // ディレクトリごとにまとめられたファイルの集計結果を表示する
+    console.log(this.groupFilesByDirectory());
+  });
+}
+
+function groupFilesByDirectory() {
+  const groupedFiles: { [directory]: [] } = {};
+
+  // ファイルをディレクトリごとにグループ化する
+  for (const fileEntry of this.filesByDirectory) {
+    const pathSegments = fileEntry.path.split("/");
+    const directory = pathSegments[0];
+    const filename = pathSegments[1];
+
+    if (!groupedFiles[directory]) {
+      groupedFiles[directory] = [];
+    }
+    groupedFiles[directory].push(filename);
+  }
+
+  // ディレクトリごとの配列を生成する
+  const result: { path: string }[] = [];
+  for (const directory of Object.keys(groupedFiles)) {
+    const directoryFiles = groupedFiles[directory];
+
+    for (const filename of directoryFiles) {
+      const fileEntry = { path: `${directory}/${filename}` };
+      result.push(fileEntry);
+    }
+  }
+
+  return result;
+}
